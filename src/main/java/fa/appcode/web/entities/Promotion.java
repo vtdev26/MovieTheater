c427c8cdc3e4011dfe2dbb0a8b8ec9ca2457397d
TruongNguyen <truongnguyen2799@gmail.com>
2021-12-02 23:20:54 +0700

    truongnguyen update promotion validate

--- src/main/java/fa/appcode/web/entities/Promotion.java
+++ src/main/java/fa/appcode/web/entities/Promotion.java
@@ -8,6 +8,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -28,21 +30,27 @@ public class Promotion {
 	private int promotionId;
 	
 	@Column(name = "detail", columnDefinition = "NVARCHAR(255)")
+	@NotEmpty(message = "{promotion.detail.empty}")
 	private String detail;
 	
 	@Column(name = "discount_level")
+	@NotEmpty(message = "{promotion.discountLevel.empty}")
+	@Pattern(regexp = "\\d+", message = "{promotion.discountLevel.invalid}")
 	private int discountLevel;
 	
 	@Column(name = "end_time", columnDefinition = "DATETIME")
+	@NotEmpty(message = "{promotion.endTime.empty}")
 	private Date endTime;
 	
 	@Column(name = "image", columnDefinition = "VARCHAR(255)")
 	private String image;
 	
 	@Column(name = "start_time",  columnDefinition = "DATETIME")
+	@NotEmpty(message = "{promotion.startTime.empty}")
 	private Date startTime;
 	
 	@Column(name = "title", columnDefinition = "VARCHAR(255)")
+	@NotEmpty(message = "{promotion.title.empty}")
 	private String title;
 
 }
